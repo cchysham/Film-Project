5e395e0b136d954ae02453a83bfc0223b9ab4063
cutesd <kimberly@kimberlycooper.me>
2018-06-29 10:36:17 -0700

    modal window

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,72 @@
 $(document).ready(function () {
-  var userInput = "";
-  $("#search-button").on("click", function (event) {
-    event.preventDefault();
-    var searchVal = $("#query").val();
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchVal + "&key=AIzaSyCu5-okTOpJ9J0XeEWjW-FHemPiCUhWJ5E"
-    $.ajax({
-      url: url,
-      method: "GET"
-    }).then(function (response) {
-      $("#film-content2").empty();
-      for (var i = 0; i < 11; i++) {
-        console.log(response);
-        console.log(response.items[i].id.videoId);
-        userInput = response.items[i].id.videoId;
-        console.log("Inside response" + userInput);
-        $("#film-content2").append(`<div class="col-4 text-center"><iframe width="420" height="315" src="https://www.youtube.com/embed/` + userInput + `"></iframe></div>`);
-      }
-    });
-  });
 
   var FilmFinder = function () {
 
-    this.start = init;
+    var vidContent = $("#vid-content");
 
-    this.welcomeView = function () {
+    this.start = init;
+    this.welView = welcomeView;
+    this.pgView = pageView;
+
+    function init() {
+      console.log("ready");
+    }
+
+    /*======================================== */
+    /*===============  SEARCH  =============== */
+    /*======================================== */
+
+    $("#search-button").on("click", searchBtnPress);
+    $("#wSearch-btn").on("click", searchBtnPress);
+
+    function searchBtnPress(e) {
+      e.preventDefault();
+      var q = $("#query").val();
+      $("#query").val('');
+      //
+      searchYT(q, 11);
+
+      //
+      pageView();
+    }
+
+
+    function searchYT(query, limit) {
+      var url = "https://www.googleapis.com/youtube/v3/search";
+      var obj = {
+        part: 'snippet',
+        key: 'AIzaSyCu5-okTOpJ9J0XeEWjW-FHemPiCUhWJ5E',
+        q: query
+      };
+      url += '?' + $.param(obj);
+      $.ajax({
+        url: url,
+        method: "GET"
+      }).then(function (response) {
+
+        console.log(response);
+        clearPage();
+        for (var i = 0; i < limit; i++) {
+          addVid(response.items[i].id.videoId);
+        }
+      });
+    }
+
+    /*======================================== */
+    /*===============  VIEW  ================= */
+    /*======================================== */
+
+    function addVid(vidID) {
+      var div = $("<div>").addClass("col-4 text-center");
+      div.append(`<iframe src="https://www.youtube.com/embed/` + vidID + `"></iframe>`);
+      vidContent.append(div);
+    }
+
+    function clearPage() {
+      vidContent.empty();
+    }
+
+    function welcomeView() {
       makeVis("login", true);
       makeVis("welcome-search", true);
       $("#content").addClass("fixed-height");
@@ -34,7 +77,7 @@ $(document).ready(function () {
       makeVis("prev-search", false);
     }
 
-    this.pageView = function () {
+    function pageView() {
       makeVis("login", false);
       makeVis("welcome-search", false);
       $("#content").removeClass("fixed-height");
@@ -45,9 +88,7 @@ $(document).ready(function () {
       makeVis("prev-search", true);
     }
 
-    function init() {
-      console.log("ready");
-    }
+
 
     function makeVis(id, val) {
       if (val)
@@ -56,6 +97,9 @@ $(document).ready(function () {
         $("#" + id).addClass("d-none");
     }
 
+    //
+    //
+    // end
   }
 
   var myFinder = new FilmFinder();
@@ -64,9 +108,9 @@ $(document).ready(function () {
   $(document).keyup(function (e) {
     console.log(e.key);
     if (e.key === "w")
-      myFinder.welcomeView();
+      myFinder.welView();
     else if (e.key === "v")
-      myFinder.pageView();
+      myFinder.pgView();
   })
 
   //
