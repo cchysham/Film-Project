a1b845fd5088d922f3c8b8589b9784cf6792c32e
cutesd <kimberly@kimberlycooper.me>
2018-06-29 13:14:30 -0700

    Added OMDB AJAX

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,18 +17,50 @@ $(document).ready(function () {
     /*======================================== */
 
     $("#search-button").on("click", searchBtnPress);
-    $("#wSearch-btn").on("click", searchBtnPress);
+    $("#wSearch-btn").on("click", searchInputPress);
+
+    function searchInputPress(e) {
+      e.preventDefault();
+      var q = $("#input-search").val();
+      $("#input-search").val('');
+      //
+      searchOMDB(q);
+      searchYT(q, 11);
+
+      //
+      pageView();
+    }
 
     function searchBtnPress(e) {
       e.preventDefault();
       var q = $("#query").val();
       $("#query").val('');
       //
+      searchOMDB(q);
       searchYT(q, 11);
 
       //
       pageView();
     }
+
+    function searchOMDB(movie) {
+      var url = "http://api.themoviedb.org/3/search/movie";
+      var obj = {
+        query: movie,
+        api_key: "67c6def7e44101cc4b977b7aa552d028"
+      }
+      url += '?' + $.param(obj);
+      // Creating an AJAX call for the specific movie button being clicked
+      $.ajax({
+        url: url,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+        addMovieInfo(response.results[0]);
+      });
+
+    }
+
 
 
     function searchYT(query, limit) {
@@ -56,6 +88,46 @@ $(document).ready(function () {
     /*===============  VIEW  ================= */
     /*======================================== */
 
+    function addMovieInfo(obj) {
+
+
+      // Creating a div to hold the movie
+      var movieDiv = $("<div class='movie'>");
+
+      // Storing the rating data
+      // var rating = response.results[0].vote_average;
+      // console.log(rating);
+      $("#bio-title").text(obj.original_title);
+      $("#bio-small").text("Rating: " + obj.vote_average);
+      // Creating an element to have the rating displayed
+      // var pOne = $("<p>").text("Rating: " + rating);
+      // Displaying the rating
+      // movieDiv.append(pOne);
+      // Storing the release year
+      // var released = response.results[0].release_date;
+      $("#bio-subtitle").text("Released: " + obj.release_date);
+      // Creating an element to hold the release year
+      // var pTwo = $("<p>").text("Released: " + released);
+      // Displaying the release year
+      // movieDiv.append(pTwo);
+      // Storing the plot
+      // var plot = response.results[0].overview;
+      $(".card-text").text(obj.overview);
+      // Creating an element to hold the plot
+      // var pThree = $("<p>").text("Plot: " + plot);
+      // Appending the plot
+      // movieDiv.append(pThree);
+      // Retrieving the URL for the image
+      // var imgURL = response.results[0].poster_path;
+      // Creating an element to hold the image
+      // var image = $("<img>").attr("src", "https://image.tmdb.org/t/p/w1280/" + imgURL);
+      $("#bio-img").attr("src", "https://image.tmdb.org/t/p/w1280/" + obj.poster_path)
+      // Appending the image
+      // movieDiv.append(image);
+      // Putting the entire movie above the previous movies
+      // $("#movies-view").prepend(movieDiv);
+    }
+
     function addVid(vidID) {
       var div = $("<div>").addClass("card m-2");
       var a = $("<a>").attr({ "data-toggle": "modal", "data-target": "#vidModal", "data-vidID": vidID });
@@ -69,8 +141,7 @@ $(document).ready(function () {
       e.preventDefault();
       var vidID = $(this).attr("data-vidID");
       console.log(vidID);
-      $(".modal-body").empty();
-      $(".modal-body").append(`<iframe width="800" height="500" src="https://www.youtube.com/embed/` + vidID + `"></iframe>`);
+      $(".modal-body").html(`<iframe width="800" height="500" src="https://www.youtube.com/embed/` + vidID + `"></iframe>`);
     }
 
     function clearPage() {
@@ -78,7 +149,7 @@ $(document).ready(function () {
     }
 
     function welcomeView() {
-      makeVis("login", true);
+      // makeVis("login", true);
       makeVis("welcome-search", true);
       $("#content").addClass("fixed-height");
       $("footer").addClass("fixed-bottom");
@@ -89,7 +160,7 @@ $(document).ready(function () {
     }
 
     function pageView() {
-      makeVis("login", false);
+      // makeVis("login", false);
       makeVis("welcome-search", false);
       $("#content").removeClass("fixed-height");
       $("footer").removeClass("fixed-bottom")
@@ -116,13 +187,13 @@ $(document).ready(function () {
   var myFinder = new FilmFinder();
   myFinder.start();
 
-  $(document).keyup(function (e) {
-    console.log(e.key);
-    if (e.key === "w")
-      myFinder.welView();
-    else if (e.key === "v")
-      myFinder.pgView();
-  })
+  // $(document).keyup(function (e) {
+  //   console.log(e.key);
+  //   if (e.key === "w")
+  //     myFinder.welView();
+  //   else if (e.key === "v")
+  //     myFinder.pgView();
+  // })
 
   //
   //
